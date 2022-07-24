a29a6a60c1797a7ecfaa9648f570e5282fff84e8
Tom Gregor <tgregor@ivsoftware.com>
2022-07-24 11:06:24 -0600

    working cleaned up

--- settings_parser/MainForm.cs
+++ settings_parser/MainForm.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Newtonsoft.Json;
 
 namespace settings_parser
 {
